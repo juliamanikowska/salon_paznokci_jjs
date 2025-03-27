041db47922b81462c21ee110c7d620ab80b05566
juliamanikowska <135166770+juliamanikowska@users.noreply.github.com>
2025-03-27 14:35:05 +0100

    slider edit

--- var/cache/prod/smarty/cache/ps_customtext/1/1/14/classic/e8/70/1d/e8701d5fec0b550aef09ad0c521208cf02b7f13d.ps_customtextps_customtext.tpl.php
+++ var/cache/prod/smarty/cache/ps_customtext/1/1/14/classic/e8/70/1d/e8701d5fec0b550aef09ad0c521208cf02b7f13d.ps_customtextps_customtext.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 4.3.4, created on 2025-03-26 10:31:06
+/* Smarty version 4.3.4, created on 2025-03-27 13:26:42
   from 'module:ps_customtextps_customtext.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.4',
-  'unifunc' => 'content_67e3c95a14b252_02156887',
+  'unifunc' => 'content_67e54402a71cc3_44183384',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -18,12 +18,11 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-function content_67e3c95a14b252_02156887 (Smarty_Internal_Template $_smarty_tpl) {
+function content_67e54402a71cc3_44183384 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div id="custom-text">
-  <h2>Custom Text Block</h2>
-<p><strong class="dark">Lorem ipsum dolor sit amet conse ctetu</strong></p>
-<p>Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+  <h2>O nas</h2>
+<p>Salon paznokci JJ's to profesjonalny salon kosmetyczny uwielbiany przez Hrabiego Ricona III Lorda Kanapy i Władcy Podwórka.</p>
 </div>
 <?php }
 }
--- var/cache/prod/smarty/cache/ps_imageslider/1/1/14/classic/3c/62/94/3c6294b651b372d9a4e3002e1345346db17da388.ps_imagesliderviewstemplateshookslider.tpl.php
+++ var/cache/prod/smarty/cache/ps_imageslider/1/1/14/classic/3c/62/94/3c6294b651b372d9a4e3002e1345346db17da388.ps_imagesliderviewstemplateshookslider.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 4.3.4, created on 2025-03-27 12:58:52
+/* Smarty version 4.3.4, created on 2025-03-27 14:12:30
   from 'module:ps_imagesliderviewstemplateshookslider.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.4',
-  'unifunc' => 'content_67e53d7c2e21a5_62595768',
+  'unifunc' => 'content_67e54ebeec39e9_56604616',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -18,7 +18,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-function content_67e53d7c2e21a5_62595768 (Smarty_Internal_Template $_smarty_tpl) {
+function content_67e54ebeec39e9_56604616 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
   <div id="carousel" data-ride="carousel" class="carousel slide" data-interval="5000" data-wrap="true" data-pause="hover" data-touch="true">
     <ol class="carousel-indicators">
@@ -29,34 +29,45 @@ function content_67e53d7c2e21a5_62595768 (Smarty_Internal_Template $_smarty_tpl)
     <ul class="carousel-inner" role="listbox" aria-label="Pokaz slajdów">
               <li class="carousel-item active" role="option" aria-hidden="false">
                       <figure>
-              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/253418d910b48dca636f962f087ae0e935621545_logo_salon_paznokci.png" alt="Salon paznokci JJ&#039;s" loading="lazy" width="1110" height="340">
+              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/253418d910b48dca636f962f087ae0e935621545_logo_salon_paznokci.png" alt="" loading="lazy" width="1110" height="340">
                               <figcaption class="caption">
-                  <h2 class="display-1 text-uppercase">Salon Paznokci JJ&#039;s laureatem!</h2>
-                  <div class="caption-description"><h3>Najlepszy salon paznokci w Poznaniu według rankingu Reksia 2025.</h3>
-<p></p></div>
+                  <h2 class="display-1 text-uppercase"></h2>
+                  <div class="caption-description"><p style="text-align:left;">Najlepszy salon paznokci w Poznaniu według rankingu Reksia 2025.</p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"></p>
+<p style="text-align:left;"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></p></div>
                 </figcaption>
                           </figure>
                   </li>
               <li class="carousel-item " role="option" aria-hidden="true">
-          <a href="https://www.prestashop-project.org">            <figure>
-              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/sample-2.jpg" alt="sample-2" loading="lazy" width="1110" height="340">
+                      <figure>
+              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/95a1300c582109c5dcbd1831269841314a6b4c09_logo_salon_paznokci2.png" alt="" loading="lazy" width="1110" height="340">
                               <figcaption class="caption">
-                  <h2 class="display-1 text-uppercase">Sample 2</h2>
-                  <div class="caption-description"><h3>EXCEPTEUR OCCAECAT</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique in tortor et dignissim. Quisque non tempor leo. Maecenas egestas sem elit</p></div>
+                  <h2 class="display-1 text-uppercase"></h2>
+                  <div class="caption-description"><p>Najlepsze</p>
+<p>opinie w</p>
+<p>Poznaniu!</p>
+<p><br /><br /></p></div>
                 </figcaption>
                           </figure>
-          </a>        </li>
+                  </li>
               <li class="carousel-item " role="option" aria-hidden="true">
-          <a href="https://www.prestashop-project.org">            <figure>
-              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/sample-3.jpg" alt="sample-3" loading="lazy" width="1110" height="340">
+                      <figure>
+              <img src="http://localhost/salon_paznokci_jjs/modules/ps_imageslider/images/87f1c7f27a9ec1f8ecabf66f991f9a346cc11f2e_logo_salon_paznokci3.png" alt="" loading="lazy" width="1110" height="340">
                               <figcaption class="caption">
-                  <h2 class="display-1 text-uppercase">Sample 3</h2>
-                  <div class="caption-description"><h3>EXCEPTEUR OCCAECAT</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique in tortor et dignissim. Quisque non tempor leo. Maecenas egestas sem elit</p></div>
+                  <h2 class="display-1 text-uppercase"></h2>
+                  <div class="caption-description"><p style="text-align:right;">Moda, trendy i estetyka</p>
+<p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></p></div>
                 </figcaption>
                           </figure>
-          </a>        </li>
+                  </li>
           </ul>
     <div class="direction" aria-label="Przyciski karuzeli">
       <a class="left carousel-control" href="#carousel" role="button" data-slide="prev" aria-label="Poprzedni">
